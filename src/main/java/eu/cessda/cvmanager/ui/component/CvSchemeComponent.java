48617a22f8f7cbcb55ba5ecda65f63b377d89915
Claus-Peter Klas <claus-peter.klas@gesis.org>
2017-11-08 15:18:52 +0100

    show the list of items found during search in a CV

--- src/main/java/eu/cessda/cvmanager/ui/component/CvSchemeComponent.java
+++ src/main/java/eu/cessda/cvmanager/ui/component/CvSchemeComponent.java
@@ -20,12 +20,12 @@ import com.vaadin.ui.Image;
 import eu.cessda.cvmanager.service.ConfigurationService;
 import eu.cessda.cvmanager.ui.view.DetailView;
 
-public class CvSchemeComponent  extends CustomComponent{
+public class CvSchemeComponent extends CustomComponent {
 	private CVScheme cvScheme;
 	private List<CVConcept> cvConcepts;
-	
+
 	private static final long serialVersionUID = 4932510587869607326L;
-	
+
 	private MCssLayout container = new MCssLayout();
 	private MHorizontalLayout hLayout = new MHorizontalLayout();
 	private MVerticalLayout vLayout = new MVerticalLayout();
@@ -35,85 +35,59 @@ public class CvSchemeComponent  extends CustomComponent{
 	private MLabel olTitle = new MLabel();
 	private MLabel desc = new MLabel();
 	private MLabel version = new MLabel();
+
+	private MLabel conceptList = new MLabel();
 	private Image logo;
 	private ConfigurationService configService;
-	
+
 	public CvSchemeComponent(CVScheme cvScheme, ConfigurationService configService) {
 		this.cvScheme = cvScheme;
 		this.configService = configService;
-		
+
 		setCompositionRoot(container);
 		initLayout();
 	}
-	
+
 	private void initLayout() {
-		enTitle
-			.withStyleName( "marginright20" )
-			.withContentMode( ContentMode.HTML );
-			//.addContextClickListener( e-> UI.getCurrent().getNavigator().navigateTo(DetailView.VIEW_NAME) );
-		olTitle.withContentMode( ContentMode.HTML );
-		//.addContextClickListener( e-> UI.getCurrent().getNavigator().navigateTo(DetailView.VIEW_NAME) );
-		desc
-			.withContentMode( ContentMode.HTML )
-			.withFullWidth();
-		version.withContentMode( ContentMode.HTML );
-		
+		enTitle.withStyleName("marginright20").withContentMode(ContentMode.HTML);
+		// .addContextClickListener( e->
+		// UI.getCurrent().getNavigator().navigateTo(DetailView.VIEW_NAME) );
+		olTitle.withContentMode(ContentMode.HTML);
+		// .addContextClickListener( e->
+		// UI.getCurrent().getNavigator().navigateTo(DetailView.VIEW_NAME) );
+		desc.withContentMode(ContentMode.HTML).withFullWidth();
+		version.withContentMode(ContentMode.HTML);
+		conceptList.withContentMode(ContentMode.HTML);
+
 		languageLayout.withFullWidth();
-		cvScheme.getLanguagesByTitle().forEach( item -> {
-			MButton langBUtton = new MButton( item.toUpperCase() );
-			langBUtton
-				.withStyleName( "langbutton" )
-				.addClickListener( e -> setContent( e.getButton().getCaption().toLowerCase() ));
-			languageLayout.add(
-				langBUtton
-			);
+		cvScheme.getLanguagesByTitle().forEach(item -> {
+			MButton langBUtton = new MButton(item.toUpperCase());
+			langBUtton.withStyleName("langbutton")
+					.addClickListener(e -> setContent(e.getButton().getCaption().toLowerCase()));
+			languageLayout.add(langBUtton);
 		});
-		
-		titleLayout
-			.withFullWidth()
-			.add(
-					enTitle,
-					olTitle
-				);
-		
-		vLayout
-			.withMargin( false )
-			.withFullWidth()
-			.add( 
-				languageLayout,
-				titleLayout,
-				desc,
-				version
-			);
-		
-		Resource res =  new ThemeResource( "img/ddi-logo-r.png");
-		if( cvScheme.getOwnerAgency() != null && !cvScheme.getOwnerAgency().isEmpty()) {
+
+		titleLayout.withFullWidth().add(enTitle, olTitle);
+
+		vLayout.withMargin(false).withFullWidth().add(languageLayout, titleLayout, desc, version, conceptList);
+
+		Resource res = new ThemeResource("img/ddi-logo-r.png");
+		if (cvScheme.getOwnerAgency() != null && !cvScheme.getOwnerAgency().isEmpty()) {
 			CVEditor agency = cvScheme.getOwnerAgency().get(0);
-			if( agency.getLogoPath() != null && !agency.getLogoPath().isEmpty())
-				res = new ThemeResource( agency.getLogoPath() );
+			if (agency.getLogoPath() != null && !agency.getLogoPath().isEmpty())
+				res = new ThemeResource(agency.getLogoPath());
 		}
 
-		logo = new Image( null, res );
-		logo.setWidth( "100" );
-		
-		hLayout
-			.withFullWidth()
-			.add( 
-				logo, 
-				vLayout
-			)
-			.withExpand(logo, 0.13f)
-			.withExpand(vLayout, 0.87f);
-			
-		
-		container
-			.withStyleName("itemcontainer")
-			.withFullWidth()
-			.add( hLayout );
-		//Initial
-		setContent( "en" );
+		logo = new Image(null, res);
+		logo.setWidth("100");
+
+		hLayout.withFullWidth().add(logo, vLayout).withExpand(logo, 0.13f).withExpand(vLayout, 0.87f);
+
+		container.withStyleName("itemcontainer").withFullWidth().add(hLayout);
+		// Initial
+		setContent("en");
 	}
-	
+
 	public Image getLogo() {
 		return logo;
 	}
@@ -125,24 +99,37 @@ public class CvSchemeComponent  extends CustomComponent{
 	public List<CVConcept> getCvElements() {
 		return cvConcepts;
 	}
+
 	public void setCvElements(List<CVConcept> cvElements) {
 		this.cvConcepts = cvElements;
 	}
-	
+
 	public MCssLayout getContainer() {
 		return container;
 	}
+
 	public void setContainer(MCssLayout container) {
 		this.container = container;
 	}
-	
-	private void setContent( String language ) {
-		enTitle.setValue( "<a href='" + configService.getServerContextPath()
-				+ "/#!" + DetailView.VIEW_NAME+ "/" + cvScheme.getContainerId() + "'>" + cvScheme.getTitleByLanguage( "en" )+ "</a>");
-		olTitle.setValue(  "<a href='" + configService.getServerContextPath()
-		+ "/#!" + DetailView.VIEW_NAME+ "/" + cvScheme.getContainerId() + "'>"  + cvScheme.getTitleByLanguage( language )  + "</a>" );
-		desc.setValue( cvScheme.getDescriptionByLanguage( language ) );
-		version.setValue( "Version: " + cvScheme.getVersion().getPublicationVersion() +(language.equals("en")? "": "_"+language) + " <a href='#'>Download</a>");
+
+	private void setContent(String language) {
+		enTitle.setValue("<a href='" + configService.getServerContextPath() + "/#!" + DetailView.VIEW_NAME + "/"
+				+ cvScheme.getContainerId() + "'>" + cvScheme.getTitleByLanguage("en") + "</a>");
+		olTitle.setValue("<a href='" + configService.getServerContextPath() + "/#!" + DetailView.VIEW_NAME + "/"
+				+ cvScheme.getContainerId() + "'>" + cvScheme.getTitleByLanguage(language) + "</a>");
+		desc.setValue(cvScheme.getDescriptionByLanguage(language));
+		version.setValue("Version: " + cvScheme.getVersion().getPublicationVersion()
+				+ (language.equals("en") ? "" : "_" + language) + " <a href='#'>Download</a>");
+
+		List<CVConcept> theConceptList = cvScheme.getConceptList();
+
+		if (theConceptList.size() > 0) {
+			String sConcepts = "Found in code(s): ";
+			for (CVConcept concept : theConceptList) {
+				sConcepts += concept.getPrefLabelByLanguage(language);
+			}
+			conceptList.setValue(sConcepts);
+		}
 	}
-	
+
 }
