cc963880d1bc54684ff7770e1ebd64e6f61e8f17
Sigit Nugraha <sigit.nugraha@gesis.org>
2018-05-03 12:00:44 +0200

    enable elasticsearch indexing and searching

--- src/test/java/eu/cessda/cvmanager/es/AggregatedPageImplTest.java
+++ src/test/java/eu/cessda/cvmanager/es/AggregatedPageImplTest.java
@@ -8,19 +8,19 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
+import org.springframework.data.elasticsearch.core.aggregation.impl.AggregatedPageImpl;
 
 public class AggregatedPageImplTest {
 
 	@Test
 	@Ignore
 	public void constructFacetedPageWithPageable() {
-//		Page<String> page = new AggregatedPageImpl<>(Arrays.asList("Test", "Test 2"), PageRequest.of(0, 2), 10);
-//
-//		assertEquals(10, page.getTotalElements());
-//		assertEquals(2, page.getNumberOfElements());
-//		assertEquals(2, page.getSize());
-//		assertEquals(5, page.getTotalPages());
+		Page<String> page = new AggregatedPageImpl<>(Arrays.asList("Test", "Test 2"), PageRequest.of(0, 2), 10);
+
+		assertEquals(10, page.getTotalElements());
+		assertEquals(2, page.getNumberOfElements());
+		assertEquals(2, page.getSize());
+		assertEquals(5, page.getTotalPages());
 	}
 
 }
