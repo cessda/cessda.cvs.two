2411b0eb640c95030de7cbdf75f6395dd0b5aeea
John Shepherdson <john.shepherdson@gmail.com>
2018-10-30 09:55:28 +0000

    Edited in line with standard deployment (now uses CLIENT variable)

--- infrastructure/gcp/cvmanager-cluster-creation.sh
+++ infrastructure/gcp/cvmanager-cluster-creation.sh
@@ -2,16 +2,38 @@
 
 #############################
 #          CESSDA           #
-#      Cluster Setup        #
+#  Generic Cluster Setup    #
 #############################
-
-# Julien Le Hericy
-# CESSDA ERIC
-# j.lehericy(at)cessda.eu
 
 
 ### Load Env Variable and set gcloud Region/Zone/Project
 source ./gcp.config > /dev/null 2>&1
+
+### some debug statements
+echo "Client: $CLIENT"
+echo "Product: $PRODUCT"
+echo "Module: $MODULE"
+echo "Environment: $ENVIRONMENT"
+
+
+### Kubernetes configuration generation ###
+sed "s/DEPLOYMENTNAME/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$CLIENT-$PRODUCT-$ENVIRONMENT/g; s/PVCNAME/$CLIENT-$PRODUCT-$ENVIRONMENT-pvc/g; s/ENVIRONMENT/$ENVIRONMENT/g" ../k8s/template-$CLIENT-$PRODUCT-$MODULE-deployment.yaml > ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
+sed "s/SERVICENAME/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$CLIENT-$PRODUCT-$ENVIRONMENT/g" ../k8s/template-$CLIENT-$PRODUCT-$MODULE-service.yaml > ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
+
+
+# Kubctl credentials setup
+gcloud container clusters get-credentials $CLIENT-$PRODUCT-$ENVIRONMENT-cc --zone=$ZONE > /dev/null 2>&1
+
+
+# Namespace Creation (not usually done here)
+if kubectl get ns $CLIENT-$PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+  then
+    echo "Namespace already exists"
+  else
+    kubectl create namespace $CLIENT-$PRODUCT-$ENVIRONMENT
+    echo "Namespace created"
+fi;
+
 
 ### Kubernetes Cluster and Namespace setup
 
@@ -24,45 +46,30 @@ if gcloud container clusters list 2> /dev/null | grep -E "$PRODUCT-$ENVIRONMENT-
     echo "Cluster created"
 fi;
 
-# Kubctl credentials setup
-gcloud container clusters get-credentials $PRODUCT-$ENVIRONMENT-cc --zone=$ZONE > /dev/null 2>&1
-
-# Namespace Creation
-if kubectl get ns $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
-  then
-    echo "Namespace already exists"
-  else
-    kubectl create namespace $PRODUCT-$ENVIRONMENT
-    echo "Namespace created"
-fi;
-
-### Kubernetes configuration generation ###
-sed "s/DEPLOYMENTNAME/$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$PRODUCT-$ENVIRONMENT/g; s/ENVIRONMENT/$ENVIRONMENT/g" ../k8s/template-deployment.yaml > ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
-sed "s/SERVICENAME/$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$PRODUCT-$ENVIRONMENT/g; s/PUBLIC_IP/$PUBLIC_IP/g" ../k8s/template-service.yaml > ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
-
-### Kubernetes Deployment ###
 
 # Deployment
-if kubectl get deployment $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+if kubectl get deployment $CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT -n $CLIENT-$PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
   then
     echo "Deployment already exists, it will be destroyed to perform the new deployment"
-    kubectl delete deployment $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1
-    kubectl create -f ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml > /dev/null 2>&1
+    kubectl delete deployment $CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT -n $CLIENT-$PRODUCT-$ENVIRONMENT > /dev/null 2>&1
+    kubectl create -f ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
     echo "Deployment created"
   else
-    kubectl create -f ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml > /dev/null 2>&1
+    kubectl create -f ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
     echo "Deployment created"
 fi;
 
+
 # Service
-if kubectl get service $PRODUCT-$MODULE-$ENVIRONMENT -n $PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+if kubectl get service $CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT -n $CLIENT-$PRODUCT-$ENVIRONMENT;
   then
     echo "Service already exists"
   else
-    kubectl create -f ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml > /dev/null 2>&1
+    kubectl create -f ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
     echo "Service created"
 fi;
 
+
 ### Clean Workspace
-rm -rf ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
-rm -rf ../k8s/$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
+rm -rf ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
+rm -rf ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
