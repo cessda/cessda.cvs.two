11e5acc6333417bb5633d2a30df9589d3f9b4762
John Shepherdson <john.shepherdson@cessda.eu>
2018-11-06 17:14:21 +0000

    Various changes to make deployment scripts more generic. Added PV and PVC details for DDI and split volumes. ENVIRONMENT is set to 'staging' in gcp.config, so that existing dev instance is not wiped out (may cause problems with service IP address though, need to check). Also Java image changed to be same as cessda.pasc.admin uses (for consistency).

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,19 +1,24 @@
 pipeline {
   environment {
     project_name = "cessda-development"
+    client = "cessda"
     app_name = "cvmanager-gui"
-//env_name = "dev"
     subdomain_name = "vocabularies-dev"
-    feSvc_name = "${app_name}-service"
+    feSvc_name = "${client}-${app_name}-service"
     image_tag = "eu.gcr.io/${project_name}/${app_name}:${env.BRANCH_NAME}-v${env.BUILD_NUMBER}"
-
-//image_tag = "eu.gcr.io/${project_name}/cessda/${app_name}:latest"
-//cluster = "cessda-cvmanager-dev-cc"
   }
 
   agent any
 
   stages {
+    stage('Check environment') {
+      steps {
+       echo "Check environment"
+       echo "project_name = ${project_name}"
+       echo "app_name = ${app_name}"
+       echo "feSvc_name = ${feSvc_name}"
+       echo "image_tag = ${image_tag}"
+   }
     stage('Prepare Application') {
       steps {
         dir('./infrastructure/gcp/') {
@@ -25,15 +30,21 @@ pipeline {
 		  steps {
         withSonarQubeEnv('cessda-sonar') {
           sh 'mvn clean install -U docker:build -DskipTests -Pdocker-compose'
+  //        sh 'mvn clean install sonar:sonar -Dsonar.projectName=$JOB_NAME -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.login=${SONAR_AUTH_TOKEN}'
           sleep 5
         }
+      }
+    }  stage('Build Docker image') {
+   		steps {
+		  echo "Build Docker image"
+                  sh("gcloud docker -- pull eu.gcr.io/cessda-development/cessda-java:latest")
+                  sh("docker build -t ${image_tag} .")
       }
     }
     stage('Push Docker image') {
       steps {
 	echo "Push Docker image"
         sh("gcloud docker -- push ${image_tag}")
-        //sh("gcloud container images add-tag ${image_tag} eu.gcr.io/${project_name}/${app_name}:latest")
         sh("gcloud container images add-tag ${image_tag} eu.gcr.io/${project_name}/${app_name}:${env.BRANCH_NAME}-latest")
 
       }
--- infrastructure/gcp/cvmanager-gui-creation.sh
+++ infrastructure/gcp/cvmanager-gui-creation.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+DISK2#!/bin/bash
 
 #############################
 #          CESSDA           #
@@ -14,44 +14,113 @@ echo "Client: $CLIENT"
 echo "Product: $PRODUCT"
 echo "Module: $MODULE"
 echo "Environment: $ENVIRONMENT"
+echo "Zone: $ZONE"
+echo "Namespace: $NAMESPACE"
+echo "Persistent disk 1: $P_DISK1"
+echo "Persistent disk 2: $P_DISK2"
 
 
 ### Kubernetes configuration generation ###
-sed "s/DEPLOYMENTNAME/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$CLIENT-$PRODUCT-$ENVIRONMENT/g; s/PVCNAME/$CLIENT-$PRODUCT-$ENVIRONMENT-pvc/g; s/ENVIRONMENT/$ENVIRONMENT/g" ../k8s/template-$CLIENT-$PRODUCT-$MODULE-deployment.yaml > ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
-sed "s/SERVICENAME/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$CLIENT-$PRODUCT-$ENVIRONMENT/g" ../k8s/template-$CLIENT-$PRODUCT-$MODULE-service.yaml > ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
+sed "s/DEPLOYMENTNAME/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$NAMESPACE/g; s/PVCNAME/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pvc/g; s/PVCNAME/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pvc/g; s/ENVIRONMENT/$ENVIRONMENT/g" ../k8s/template-$CLIENT-$PRODUCT-$MODULE-deployment.yaml > ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
+sed "s/SERVICENAME/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT/g; s/NAMESPACE/$NAMESPACE/g" ../k8s/template-$CLIENT-$PRODUCT-$MODULE-service.yaml > ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
 
 
 # Kubctl credentials setup
 gcloud container clusters get-credentials $CLIENT-$PRODUCT-$ENVIRONMENT-cc --zone=$ZONE > /dev/null 2>&1
 
 
-# Namespace Creation (not usually done here)
-if kubectl get ns $CLIENT-$PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+### DDI Storage Setup ###
+
+# Disk Creation (DDI)
+if gcloud compute disks list 2> /dev/null | grep -E "$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-disk.*READY" > /dev/null 2>&1;
   then
-    echo "Namespace already exists"
+    echo "$P_DISK1 disk already exists"
   else
-    kubectl create namespace $CLIENT-$PRODUCT-$ENVIRONMENT
-    echo "Namespace created"
+    gcloud compute disks create $CLIENT-$PRODUCT-$ENVIRONMENT-$P_DISK1-disk --size 10GB > /dev/null 2>&1
+    echo "$P_DISK1 disk created"
+fi;
+
+#Persistent Volume Creation (DDI)
+if kubectl get pv -n $CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT 2> /dev/null | grep -E "$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pv" > /dev/null 2>&1;
+  then
+    echo "$P_DISK1 Persistent Volume already exist"
+  else
+    sed "s/PVNAME/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pv/g; s/NAMESPACE/$NAMESPACE/g; s/DISKNAME/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-disk/g" ../k8s/template-$CLIENT-$PRODUCT-$P_DISK1-pv.yaml > ../k8s/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pv.yaml
+    kubectl create -f ../k8s/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pv.yaml > /dev/null 2>&1
+    rm -rf ../k8s/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pv.yaml
+    echo "$P_DISK1 Persistent Volume created"
+fi;
+
+#Persistent Volume Claim (DDI)
+if kubectl get pvc -n $CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT 2> /dev/null | grep -E "$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pvc" > /dev/null 2>&1;
+  then
+    echo "$P_DISK1 Persistent Volume Claim already exist"
+  else
+    sed "s/PVNAME/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pv/g; s/NAMESPACE/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT/g; s/PVCNAME/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pvc/g" ../k8s/template-$CLIENT-$PRODUCT-$P_DISK1-pvc.yaml > ../k8s/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pvc.yaml
+    kubectl create -f ../k8s/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pvc.yaml > /dev/null 2>&1
+    rm -rf ../k8s/$CLIENT-$PRODUCT-$P_DISK1-$ENVIRONMENT-pvc.yaml
+    echo "$P_DISK1 Persistent Volume Claim created"
+fi;
+
+### Split Storage Setup ###
+
+# Disk Creation (Split)
+f gcloud compute disks list 2> /dev/null | grep -E "$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-disk.*READY" > /dev/null 2>&1;
+  then
+    echo "$P_DISK2 disk already exists"
+  else
+    gcloud compute disks create $CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-disk --size 10GB > /dev/null 2>&1
+    echo "$P_DISK2 disk created"
+
+#Persistent Volume Creation (Split)
+f kubectl get pv -n $CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT 2> /dev/null | grep -E "$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pv" > /dev/null 2>&1;
+  then
+    echo "$P_DISK2 Persistent Volume already exist"
+  else
+    sed "s/PVNAME/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pv/g; s/NAMESPACE/$NAMESPACE/g; s/DISKNAME/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-disk/g" ../k8s/template-$CLIENT-$PRODUCT-$P_DISK2-pv.yaml > ../k8s/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pv.yaml
+    kubectl create -f ../k8s/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pv.yaml > /dev/null 2>&1
+    rm -rf ../k8s/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pv.yaml
+    echo "$P_DISK2 Persistent Volume created"
+fi;
+
+#Persistent Volume Claim (Split)
+if kubectl get pvc -n $CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT 2> /dev/null | grep -E "$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pvc" > /dev/null 2>&1;
+  then
+    echo "$P_DISK2 Persistent Volume Claim already exist"
+  else
+    sed "s/PVNAME/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pv/g; s/NAMESPACE/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT/g; s/PVCNAME/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pvc/g" ../k8s/template-$CLIENT-$PRODUCT-$P_DISK2-pvc.yaml > ../k8s/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pvc.yaml
+    kubectl create -f ../k8s/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pvc.yaml > /dev/null 2>&1
+    rm -rf ../k8s/$CLIENT-$PRODUCT-$P_DISK2-$ENVIRONMENT-pvc.yaml
+    echo "$P_DISK2 Persistent Volume Claim created"
 fi;
 
 
 ### Kubernetes Cluster and Namespace setup
 
 # Cluster Creation
-if gcloud container clusters list 2> /dev/null | grep -E "$PRODUCT-$ENVIRONMENT-cc.*RUNNING" > /dev/null 2>&1;
-  then
-    echo "Cluster already exists"
-  else
-    gcloud container clusters create $PRODUCT-$ENVIRONMENT-cc --network $NET --subnetwork $SUBNET --num-nodes 1 --machine-type n1-standard-1 --scopes "cloud-platform" --disk-size 50 --zone $ZONE --cluster-version=1.8.2-gke.0 --labels=env=$ENVIRONMENT,project=$PRODUCT
-    echo "Cluster created"
-fi;
+# # # # # # # # # # # # # if gcloud container clusters list 2> /dev/null | grep -E "$CLIENT-$PRODUCT-$ENVIRONMENT-cc.*RUNNING" > /dev/null 2>&1;
+# # # # # # # # # # # #   then
+# # # # # # # # # # #     echo "Cluster already exists"
+# # # # # # # # # #   else
+# # # # # # # # #     gcloud container clusters create $CLIENT-$PRODUCT-$ENVIRONMENT-cc --network $NET --subnetwork $SUBNET --num-nodes 1 --machine-type n1-standard-1 --scopes "cloud-platform" --disk-size 50 --zone $ZONE --cluster-version=1.8.2-gke.0 --labels=env=$ENVIRONMENT,project=$PRODUCT
+# # # # # # # #     echo "Cluster created"
+# # # # # # # fi;
+
+# Namespace Creation
+# # # # # # if kubectl get ns $NAMESPACE > /dev/null 2>&1;
+# # # # #   then
+# # # #   else
+# # #     kubectl create namespace $NAMESPACE
+# #     echo "$NAMESPACE Namespace created"
+# fi;
+
 
 
 # Deployment
-if kubectl get deployment $CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT -n $CLIENT-$PRODUCT-$ENVIRONMENT > /dev/null 2>&1;
+if kubectl get deployment $CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT -n $NAMESPACE > /dev/null 2>&1;
   then
     echo "Deployment already exists, it will be destroyed to perform the new deployment"
-    kubectl delete deployment $CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT -n $CLIENT-$PRODUCT-$ENVIRONMENT > /dev/null 2>&1
+    kubectl delete deployment $CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT -n $NAMESPACE > /dev/null 2>&1
     kubectl create -f ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-deployment.yaml
     echo "Deployment created"
   else
@@ -61,12 +130,12 @@ fi;
 
 
 # Service
-if kubectl get service $CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT -n $CLIENT-$PRODUCT-$ENVIRONMENT;
+if kubectl get service $CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT -n $NAMESPACE;
   then
-    echo "Service already exists"
+    echo "$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT Service already exists"
   else
     kubectl create -f ../k8s/$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT-service.yaml
-    echo "Service created"
+    echo "$CLIENT-$PRODUCT-$MODULE-$ENVIRONMENT Service created"
 fi;
 
 
