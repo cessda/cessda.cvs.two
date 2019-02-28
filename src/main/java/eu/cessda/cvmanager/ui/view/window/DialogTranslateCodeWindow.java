90ae3f6d01b9b867591a4dd74cc48f51671ee13c
Sigit Nugraha <sigit.nugraha@gesis.org>
2019-02-28 14:43:25 +0100

    fix notation missing on save concept translation

--- src/main/java/eu/cessda/cvmanager/ui/view/window/DialogTranslateCodeWindow.java
+++ src/main/java/eu/cessda/cvmanager/ui/view/window/DialogTranslateCodeWindow.java
@@ -244,7 +244,7 @@ public class DialogTranslateCodeWindow extends MWindow {
 		
 		// store the code
 		WorkspaceManager.saveCode(vocabulary, version, code, null, concept, 
-				concept.getNotation(), preferedLabel.getValue(), description.getValue());
+				code.getNotation(), preferedLabel.getValue(), description.getValue());
 
 		// save change log
 		WorkspaceManager.storeChangeLog(vocabulary, version, "TL code added", concept.getNotation());
