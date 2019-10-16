fa98d0ebe33cf7eab3b3b4a77a307e7d6a9ba6b4
Klas <claus-peter.klas@gesis.org>
2019-10-16 11:30:06 +0200

    cleanup wrt sonarlint: blocker

--- src/main/java/eu/cessda/cvmanager/export/utils/SaxParserUtils.java
+++ src/main/java/eu/cessda/cvmanager/export/utils/SaxParserUtils.java
@@ -4,9 +4,10 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.Set;
 
+import javax.xml.XMLConstants;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
-import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
@@ -26,62 +27,79 @@ public class SaxParserUtils {
 
 	private static final Logger log = LoggerFactory.getLogger(SaxParserUtils.class);
 
-	public static String filterSkosDoc(Set<String> filteredTag, Set<String> filteredLanguage, String xmlString){
+	private SaxParserUtils() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	public static String filterSkosDoc(Set<String> filteredTag, Set<String> filteredLanguage, String xmlString) {
 		XMLReader xr;
 		try {
-			xr = new XMLFilterImpl(XMLReaderFactory.createXMLReader()) {
-			    private boolean skip;
+			xr = xmlReader(filteredTag, filteredLanguage);
 
-			    @Override
-			    public void startElement(String uri, String localName, String qName, Attributes atts)
-			            throws SAXException {
-			    	// filter tag
-			        if ( filteredTag != null && filteredTag.contains( qName ))
-			        	skip = true;
-			        else
-			        	skip = false;
-			        
-			        // filter language attribute
-			        if( !skip && filteredLanguage != null) {
-			        	String langValue =  atts.getValue("xml:lang");
-			        	if( langValue != null && filteredLanguage.contains(langValue))
-			        		skip = true;
-			        }
+			Source src = new SAXSource(xr, new InputSource(new StringReader(xmlString)));
+			StringWriter sw = new StringWriter();
+			Result res = new StreamResult(sw);
+			try {
+				TransformerFactory factory = TransformerFactory.newInstance();
+				factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+				Transformer transformer = factory.newTransformer();
 
-		            if (!skip)
-		                super.startElement(uri, localName, qName, atts);
-		        
-			    }
-
-			    public void endElement(String uri, String localName, String qName) throws SAXException {
-			    	if ( filteredTag != null && filteredTag.contains( qName ))
-			        	skip = true;
-			    	
-			       if (!skip)
-			            super.endElement(uri, localName, qName);
-			       skip=false;
-			    }
-
-			    @Override
-			    public void characters(char[] ch, int start, int length) throws SAXException {
-			        if (!skip) {
-			            super.characters(ch, start, length);
-			       }
-			    }
-			};
-			Source src = new SAXSource(xr, new InputSource( new StringReader(xmlString)));
-			StringWriter sw  =new StringWriter();
-		    Result res = new StreamResult( sw );
-		    try {
-				TransformerFactory.newInstance().newTransformer().transform(src, res);
+				transformer.transform(src, res);
 				return sw.toString();
 			} catch (TransformerException | TransformerFactoryConfigurationError e) {
 				log.error(e.getMessage(), e);
 			}
 		} catch (SAXException e) {
-			// TODO Auto-generated catch block
+
 			log.error(e.getMessage(), e);
 		}
-	    return null;
+		return null;
+	}
+
+	private static XMLReader xmlReader(Set<String> filteredTag, Set<String> filteredLanguage) throws SAXException {
+		XMLReader xr;
+		XMLReader xmlReaderfactory = XMLReaderFactory.createXMLReader();
+		xmlReaderfactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+		xr = new XMLFilterImpl(xmlReaderfactory) {
+			private boolean skip;
+
+			@Override
+			public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
+				// filter tag
+				if (filteredTag != null && filteredTag.contains(qName))
+					skip = true;
+				else
+					skip = false;
+
+				// filter language attribute
+				if (!skip && filteredLanguage != null) {
+					String langValue = atts.getValue("xml:lang");
+					if (langValue != null && filteredLanguage.contains(langValue))
+						skip = true;
+				}
+
+				if (!skip)
+					super.startElement(uri, localName, qName, atts);
+
+			}
+
+			@Override
+			public void endElement(String uri, String localName, String qName) throws SAXException {
+				if (filteredTag != null && filteredTag.contains(qName))
+					skip = true;
+
+				if (!skip)
+					super.endElement(uri, localName, qName);
+				skip = false;
+			}
+
+			@Override
+			public void characters(char[] ch, int start, int length) throws SAXException {
+				if (!skip) {
+					super.characters(ch, start, length);
+				}
+			}
+		};
+		return xr;
 	}
 }
