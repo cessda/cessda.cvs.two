2425025de46d6ba853e6c1f4181210dbbc11c6c4
Sigit Nugraha <sigit.nugraha@gesis.org>
2019-11-05 22:58:00 +0100

    #161 AUP to swagger page

--- src/main/java/eu/cessda/cvmanager/config/SwaggerConfiguration.java
+++ src/main/java/eu/cessda/cvmanager/config/SwaggerConfiguration.java
@@ -7,9 +7,13 @@ import com.google.common.base.Predicates;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -25,6 +29,19 @@ public class SwaggerConfiguration
 				.paths( PathSelectors.regex( "/.*" ) )
 				.paths( Predicates.not(PathSelectors.regex("/error.*")) )
 				.paths( Predicates.not(PathSelectors.regex("/actuator.*")) )
-				.build();
+				.build()
+				.apiInfo(getApiInformation());
+	}
+
+	private ApiInfo getApiInformation(){
+		return new ApiInfo("CVS REST APIs",
+				"List of CVS rest APIs",
+				"1.0",
+				"https://www.cessda.eu/Acceptable-Use-Policy",
+				new Contact("CESSDA", "https://www.cessda.eu/", "cessda@cessda.eu"),
+				"Apache 2.0",
+				"http://www.apache.org/licenses/LICENSE-2.0",
+				Collections.emptyList()
+		);
 	}
 }
