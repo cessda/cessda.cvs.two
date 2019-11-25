e20c9982ace57c0e34c58cd4193ea3b02b4f2c7b
GESIS\ho <Oliver.Hopt@gesis.org>
2019-11-25 14:49:07 +0100

    formatting and cleaning

--- src/main/java/eu/cessda/cvmanager/domain/Vocabulary.java
+++ src/main/java/eu/cessda/cvmanager/domain/Vocabulary.java
@@ -1,152 +1,147 @@
 package eu.cessda.cvmanager.domain;
 
-import javax.persistence.*;
-import javax.validation.constraints.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
-import org.gesis.wts.domain.enumeration.Language;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import eu.cessda.cvmanager.domain.Version;
-import eu.cessda.cvmanager.domain.enumeration.Status;
-
-import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Objects;
 
 /**
  * A Vocabulary.
  */
 @Entity
-@Table(name = "vocabulary")
-@Document(indexName = "vocabulary")
-public class Vocabulary extends VocabularyBase{
-    
+@Table( name = "vocabulary" )
+@Document( indexName = "vocabulary" )
+public class Vocabulary extends VocabularyBase
+{
+
 	private static final long serialVersionUID = 1L;
-	
-    // both sl and tl statuses
-    @Column(name = "statuses")
-    @ElementCollection( targetClass=String.class )
-    @Field(type = FieldType.Keyword)
-    private Set<String> statuses;
-    
-    @Column(name = "restrict_roles")
-    @ElementCollection( targetClass=String.class )
-    @Field(type = FieldType.Keyword)
-    private Set<String> restrictRoles;
-    
-//    @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH })
-//    @JoinTable(name = "vocabulary_version",
-//               joinColumns = @JoinColumn(name="vocabulary_id", referencedColumnName="id"),
-//               inverseJoinColumns = @JoinColumn(name="version_id", referencedColumnName="id"))
-//    private Set<Version> versions = new HashSet<>();
-    
-    @OneToMany(mappedBy = "vocabulary")
-    private Set<Version> versions = new HashSet<>();
-    
-	
-	public Set<Version> getVersions() {
+
+	// both sl and tl statuses
+	@Column( name = "statuses" )
+	@ElementCollection( targetClass = String.class )
+	@Field( type = FieldType.Keyword )
+	private Set<String> statuses;
+
+	@Column( name = "restrict_roles" )
+	@ElementCollection( targetClass = String.class )
+	@Field( type = FieldType.Keyword )
+	private Set<String> restrictRoles;
+
+	@OneToMany( mappedBy = "vocabulary" )
+	private Set<Version> versions = new HashSet<>();
+
+	public Set<Version> getVersions()
+	{
 		return versions;
 	}
 
-	public void setVersions(Set<Version> versions) {
+	public void setVersions( Set<Version> versions )
+	{
 		this.versions = versions;
 	}
-	
 
-	public Set<String> getRestrictRoles() {
+	public Set<String> getRestrictRoles()
+	{
 		return restrictRoles;
 	}
 
-	public void setRestrictRoles(Set<String> restrictRoles) {
+	public void setRestrictRoles( Set<String> restrictRoles )
+	{
 		this.restrictRoles = restrictRoles;
 	}
 
-	public Set<String> getStatuses() {
+	public Set<String> getStatuses()
+	{
 		return statuses;
 	}
 
-	public void setStatuses(Set<String> statuses) {
+	public void setStatuses( Set<String> statuses )
+	{
 		this.statuses = statuses;
 	}
 
+	@Override
+	public boolean equals( Object o )
+	{
+		if ( this == o )
+		{
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() )
+		{
+			return false;
+		}
+		Vocabulary vocabulary = (Vocabulary) o;
+		if ( vocabulary.getId() == null || getId() == null )
+		{
+			return false;
+		}
+		return Objects.equals( getId(), vocabulary.getId() );
+	}
 
 	@Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Vocabulary vocabulary = (Vocabulary) o;
-        if (vocabulary.getId() == null || getId() == null) {
-            return false;
-        }
-        return Objects.equals(getId(), vocabulary.getId());
-    }
+	public int hashCode()
+	{
+		return Objects.hashCode( getId() );
+	}
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(getId());
-    }
-
-    @Override
-    public String toString() {
-        return "Vocabulary{" +
-            "id=" + getId() +
-            ", uri='" + getUri() + "'" +
-            ", versionNumber='" + getVersionNumber() + "'" +
-            ", archived='" + isArchived() + "'" +
-            ", withdrawn='" + isWithdrawn() + "'" +
-            ", discoverable='" + isDiscoverable() + "'" +
-            ", sourceLanguage='" + getSourceLanguage() + "'" +
-            ", agencyName='" + getAgencyName() + "'" +
-            ", titleCs='" + getTitleCs() + "'" +
-            ", definitionCs='" + getDefinitionCs() + "'" +
-            ", titleDa='" + getTitleDa() + "'" +
-            ", definitionDa='" + getDefinitionDa() + "'" +
-            ", titleNl='" + getTitleNl() + "'" +
-            ", definitionNl='" + getDefinitionNl() + "'" +
-            ", titleEn='" + getTitleEn() + "'" +
-            ", definitionEn='" + getDefinitionEn() + "'" +
-            ", titleFi='" + getTitleFi() + "'" +
-            ", definitionFi='" + getDefinitionFi() + "'" +
-            ", titleFr='" + getTitleFr() + "'" +
-            ", definitionFr='" + getDefinitionFr() + "'" +
-            ", titleDe='" + getTitleDe() + "'" +
-            ", definitionDe='" + getDefinitionDe() + "'" +
-            ", titleEl='" + getTitleEl() + "'" +
-            ", definitionEl='" + getDefinitionEl() + "'" +
-            ", titleHu='" + getTitleHu() + "'" +
-            ", definitionHu='" + getDefinitionHu() + "'" +
-            ", titleIt='" + getTitleIt() + "'" +
-            ", definitionIt='" + getDefinitionIt() + "'" +
-            ", titleLt='" + getTitleLt() + "'" +
-            ", definitionLt='" + getDefinitionLt() + "'" +
-            ", titleNo='" + getTitleNo() + "'" +
-            ", definitionNo='" + getDefinitionNo() + "'" +
-            ", titlePt='" + getTitlePt() + "'" +
-            ", definitionPt='" + getDefinitionPt() + "'" +
-            ", titleRo='" + getTitleRo() + "'" +
-            ", definitionRo='" + getDefinitionRo() + "'" +
-            ", titleSk='" + getTitleSk() + "'" +
-            ", definitionSk='" + getDefinitionSk() + "'" +
-            ", titleSl='" + getTitleSl() + "'" +
-            ", definitionSl='" + getDefinitionSl() + "'" +
-            ", titleEs='" + getTitleEs() + "'" +
-            ", definitionEs='" + getDefinitionEs() + "'" +
-            ", titleSv='" + getTitleSv() + "'" +
-            ", definitionSv='" + getDefinitionSv() + "'" +
-            "}";
-    }
+	@Override
+	public String toString()
+	{
+		return "Vocabulary{" +
+				"id=" + getId() +
+				", uri='" + getUri() + "'" +
+				", versionNumber='" + getVersionNumber() + "'" +
+				", archived='" + isArchived() + "'" +
+				", withdrawn='" + isWithdrawn() + "'" +
+				", discoverable='" + isDiscoverable() + "'" +
+				", sourceLanguage='" + getSourceLanguage() + "'" +
+				", agencyName='" + getAgencyName() + "'" +
+				", titleCs='" + getTitleCs() + "'" +
+				", definitionCs='" + getDefinitionCs() + "'" +
+				", titleDa='" + getTitleDa() + "'" +
+				", definitionDa='" + getDefinitionDa() + "'" +
+				", titleNl='" + getTitleNl() + "'" +
+				", definitionNl='" + getDefinitionNl() + "'" +
+				", titleEn='" + getTitleEn() + "'" +
+				", definitionEn='" + getDefinitionEn() + "'" +
+				", titleFi='" + getTitleFi() + "'" +
+				", definitionFi='" + getDefinitionFi() + "'" +
+				", titleFr='" + getTitleFr() + "'" +
+				", definitionFr='" + getDefinitionFr() + "'" +
+				", titleDe='" + getTitleDe() + "'" +
+				", definitionDe='" + getDefinitionDe() + "'" +
+				", titleEl='" + getTitleEl() + "'" +
+				", definitionEl='" + getDefinitionEl() + "'" +
+				", titleHu='" + getTitleHu() + "'" +
+				", definitionHu='" + getDefinitionHu() + "'" +
+				", titleIt='" + getTitleIt() + "'" +
+				", definitionIt='" + getDefinitionIt() + "'" +
+				", titleLt='" + getTitleLt() + "'" +
+				", definitionLt='" + getDefinitionLt() + "'" +
+				", titleNo='" + getTitleNo() + "'" +
+				", definitionNo='" + getDefinitionNo() + "'" +
+				", titlePt='" + getTitlePt() + "'" +
+				", definitionPt='" + getDefinitionPt() + "'" +
+				", titleRo='" + getTitleRo() + "'" +
+				", definitionRo='" + getDefinitionRo() + "'" +
+				", titleSk='" + getTitleSk() + "'" +
+				", definitionSk='" + getDefinitionSk() + "'" +
+				", titleSl='" + getTitleSl() + "'" +
+				", definitionSl='" + getDefinitionSl() + "'" +
+				", titleEs='" + getTitleEs() + "'" +
+				", definitionEs='" + getDefinitionEs() + "'" +
+				", titleSv='" + getTitleSv() + "'" +
+				", definitionSv='" + getDefinitionSv() + "'" +
+				"}";
+	}
 }
