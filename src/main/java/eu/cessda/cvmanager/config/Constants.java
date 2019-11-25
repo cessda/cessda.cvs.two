9eda0e8c873824044fb611a83165824e666d0437
GESIS\ho <Oliver.Hopt@gesis.org>
2019-11-25 14:20:35 +0100

    major formating and fixing sonar vulnarability on fields to be protected

--- src/main/java/eu/cessda/cvmanager/config/Constants.java
+++ src/main/java/eu/cessda/cvmanager/config/Constants.java
@@ -3,31 +3,33 @@ package eu.cessda.cvmanager.config;
 /**
  * Application constants.
  */
-public final class Constants {
+public final class Constants
+{
 
-    // Regex for acceptable logins
-    public static final String LOGIN_REGEX = "^[_'.@A-Za-z0-9-]*$";
+	// Regex for acceptable logins
+	public static final String LOGIN_REGEX = "^[_'.@A-Za-z0-9-]*$";
 
-    public static final String DEFAULT_LANGUAGE = "en";
-    
-    public static final String NOTATION_REGEX= "[^A-Za-z0-9-+:]";
-    
-    // Role
-    public static final String[] USER_ROLE = {
-    		"ROLE_ADMIN_AGENCY",			// agencies superadmin
-    		"ROLE_ADMIN_TECHNICAL",		// system technical admin
-    		"ROLE_ADMIN"};
-    
-    public static final String[] USER_AGENCY_ROLE = {
-    		"ROLE_AGENCY_VIEWER",
-    		"ROLE_AGENCY_CONTRIBUTOR", 	// SL /& TL (depends on language right assigned)
-    		"ROLE_AGENCY_AUTHOR", 		// SL /& TL (depends on language right assigned)
-    		"ROLE_AGENCY_AUTHOR_ADMIN"   // SL & TL
-    		};
+	public static final String DEFAULT_LANGUAGE = "en";
 
-    public static final String REQUIRED = "required";
-    public static final String REQUIRED_FIELD_INFO = "* required field, require an input with at least 2 characters";
-    
-    private Constants() {
-    }
+	public static final String NOTATION_REGEX = "[^A-Za-z0-9-+:]";
+
+	// Role
+	protected static final String[] USER_ROLE = {
+			"ROLE_ADMIN_AGENCY", // agencies superadmin
+			"ROLE_ADMIN_TECHNICAL", // system technical admin
+			"ROLE_ADMIN" };
+
+	protected static final String[] USER_AGENCY_ROLE = {
+			"ROLE_AGENCY_VIEWER",
+			"ROLE_AGENCY_CONTRIBUTOR", // SL /& TL (depends on language right assigned)
+			"ROLE_AGENCY_AUTHOR", // SL /& TL (depends on language right assigned)
+			"ROLE_AGENCY_AUTHOR_ADMIN" // SL & TL
+	};
+
+	public static final String REQUIRED = "required";
+	public static final String REQUIRED_FIELD_INFO = "* required field, require an input with at least 2 characters";
+
+	private Constants()
+	{
+	}
 }
