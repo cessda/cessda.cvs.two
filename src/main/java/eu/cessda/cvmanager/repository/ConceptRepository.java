96f269163fbfde3854b3e245d9ef3eb4a15e402c
GESIS\ho <Oliver.Hopt@gesis.org>
2019-11-25 16:35:18 +0100

    Formatting and cleaning

--- src/main/java/eu/cessda/cvmanager/repository/ConceptRepository.java
+++ src/main/java/eu/cessda/cvmanager/repository/ConceptRepository.java
@@ -1,31 +1,29 @@
 package eu.cessda.cvmanager.repository;
 
-import eu.cessda.cvmanager.domain.Concept;
-import eu.cessda.cvmanager.service.dto.ConceptDTO;
-
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-import org.springframework.data.jpa.repository.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.util.Streamable;
+import org.springframework.stereotype.Repository;
 
+import eu.cessda.cvmanager.domain.Concept;
 
 /**
  * Spring Data JPA repository for the Concept entity.
  */
-@SuppressWarnings("unused")
+@SuppressWarnings( "unused" )
 @Repository
-public interface ConceptRepository extends JpaRepository<Concept, Long> {
+public interface ConceptRepository extends JpaRepository<Concept, Long>
+{
 
 	@Query( "select c from Concept c where c.codeId = :codeId" )
-	List<Concept> findAllByCode(@Param("codeId") Long codeId);
+	List<Concept> findAllByCode( @Param( "codeId" ) Long codeId );
 
 	@Query( "select c from Concept c where c.codeId = :codeId and c.notation = :notation" )
-	Concept findOneByCodeNotationAndId(@Param("notation") String notation,@Param("codeId") Long codeId);
+	Concept findOneByCodeNotationAndId( @Param( "notation" ) String notation, @Param( "codeId" ) Long codeId );
 
 	@Query( "select c from Concept c where c.version.id = :versionId" )
-	List<Concept> findByVersion(@Param("versionId") Long versionId);
+	List<Concept> findByVersion( @Param( "versionId" ) Long versionId );
 
 }
