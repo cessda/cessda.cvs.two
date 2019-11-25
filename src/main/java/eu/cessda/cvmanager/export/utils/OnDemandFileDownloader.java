05146affb1c2d805777681609f1004b8d3c1816d
GESIS\ho <Oliver.Hopt@gesis.org>
2019-11-25 15:26:58 +0100

    cleaning and formatting

--- src/main/java/eu/cessda/cvmanager/export/utils/OnDemandFileDownloader.java
+++ src/main/java/eu/cessda/cvmanager/export/utils/OnDemandFileDownloader.java
@@ -9,35 +9,40 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinResponse;
 
 /**
- * This specializes {@link FileDownloader} in a way, such that both the file name and content can be determined
- * on-demand, i.e. when the user has clicked the component.
+ * This specializes {@link FileDownloader} in a way, such that both the file name and content can be
+ * determined on-demand, i.e. when the user has clicked the component.
  * {@link https://vaadin.com/docs/v8/framework/articles/LettingTheUserDownloadAFile.html}
  */
-public class OnDemandFileDownloader extends FileDownloader {
+public class OnDemandFileDownloader extends FileDownloader
+{
 
-  /**
-   * Provide both the {@link StreamSource} and the filename in an on-demand way.
-   */
-  public interface OnDemandStreamResource extends StreamSource {
-    String getFilename ();
-  }
+	/**
+	 * Provide both the {@link StreamSource} and the filename in an on-demand way.
+	 */
+	public interface OnDemandStreamResource extends StreamSource
+	{
+		String getFilename();
+	}
 
-  private static final long serialVersionUID = 1L;
-  private final OnDemandStreamResource onDemandStreamResource;
+	private static final long serialVersionUID = 1L;
+	private final OnDemandStreamResource onDemandStreamResource;
 
-  public OnDemandFileDownloader (OnDemandStreamResource onDemandStreamResource) {
-    super(new StreamResource(onDemandStreamResource, ""));
-    this.onDemandStreamResource = onDemandStreamResource;
-  }
+	public OnDemandFileDownloader( OnDemandStreamResource onDemandStreamResource )
+	{
+		super( new StreamResource( onDemandStreamResource, "" ) );
+		this.onDemandStreamResource = onDemandStreamResource;
+	}
 
-  @Override
-  public boolean handleConnectorRequest (VaadinRequest request, VaadinResponse response, String path)
-      throws IOException {
-    getResource().setFilename(onDemandStreamResource.getFilename());
-    return super.handleConnectorRequest(request, response, path);
-  }
+	@Override
+	public boolean handleConnectorRequest( VaadinRequest request, VaadinResponse response, String path )
+			throws IOException
+	{
+		getResource().setFilename( onDemandStreamResource.getFilename() );
+		return super.handleConnectorRequest( request, response, path );
+	}
 
-  private StreamResource getResource () {
-    return (StreamResource) this.getResource("dl");
-  }
+	private StreamResource getResource()
+	{
+		return (StreamResource) this.getResource( "dl" );
+	}
 }
