9b8df5beeaa643d032a25216f4b6a12110efb488
GESIS\ho <Oliver.Hopt@gesis.org>
2019-11-25 16:27:22 +0100

    neccessary methods for equals API

--- src/main/java/eu/cessda/cvmanager/export/utils/OnDemandFileDownloader.java
+++ src/main/java/eu/cessda/cvmanager/export/utils/OnDemandFileDownloader.java
@@ -45,4 +45,16 @@ public class OnDemandFileDownloader extends FileDownloader
 	{
 		return (StreamResource) this.getResource( "dl" );
 	}
+
+	@Override
+	public boolean equals( Object obj )
+	{
+		return super.equals( obj );
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return super.hashCode();
+	}
 }
