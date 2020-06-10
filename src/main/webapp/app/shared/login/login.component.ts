6c7dd3c3ffc2b7cc9d9e5dd39fcb7af163e91b37
Sigit Nugraha <sigit.nugraha@gesis.org>
2020-06-10 12:51:46 +0200

    Fix login-window jump to bottom

--- src/main/webapp/app/shared/login/login.component.ts
+++ src/main/webapp/app/shared/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginModalComponent implements AfterViewInit {
 
   ngAfterViewInit(): void {
     if (this.username) {
-      this.username.nativeElement.focus();
+      setTimeout(() => this.username!.nativeElement.focus(), 0);
     }
   }
 
