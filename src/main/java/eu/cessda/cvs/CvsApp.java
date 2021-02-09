9969212c96826457a254a7a84cc915751ea788bd
Sigit Nugraha <sigit.nugraha@gesis.org>
2021-02-09 10:46:34 +0100

    Add Apache2.0 License for all Java files under package eu.cessds.cvs

--- src/main/java/eu/cessda/cvs/CvsApp.java
+++ src/main/java/eu/cessda/cvs/CvsApp.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright © 2017-2021 CESSDA ERIC (support@cessda.eu)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
 package eu.cessda.cvs;
 
 import eu.cessda.cvs.config.ApplicationProperties;
@@ -62,6 +75,7 @@ public class CvsApp {
      *
      * @param args the command line arguments.
      */
+    @SuppressWarnings("squid:S4823") // no security hotspot, the args are properly handled
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(CvsApp.class);
         DefaultProfileUtil.addDefaultProfile(app);
