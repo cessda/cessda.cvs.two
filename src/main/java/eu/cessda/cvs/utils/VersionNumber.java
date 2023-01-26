4b94d61d7ea1258d83500aa95e6cb3145f423ee9
Stefan Dlugolinsky <stefan.dlugolinsky@savba.sk>
2023-01-26 14:06:25 +0100

    #283 - removed redundant exception declaration

--- src/main/java/eu/cessda/cvs/utils/VersionNumber.java
+++ src/main/java/eu/cessda/cvs/utils/VersionNumber.java
@@ -11,7 +11,6 @@ import org.slf4j.LoggerFactory;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -39,7 +38,7 @@ public class VersionNumber implements Comparable<VersionNumber>, Serializable {
 
         @Override
         public VersionNumber deserialize(JsonParser p, DeserializationContext ctxt)
-                throws IOException, JsonProcessingException {
+                throws IOException {
             try { 
                 return VersionNumber.fromString(p.getText()); 
             } 
