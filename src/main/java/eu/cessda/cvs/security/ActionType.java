7b05180188a631395efebbb5f0f51a8c32bd20e6
Matthew Morris <matthew.morris@cessda.eu>
2023-03-15 08:30:19 +0100

    Use an EnumSet rather than a HashSet to store the roles in the ActionType enum, cache the capitalised ISO set
    For enums, EnumSets are a more compact representation as well as faster in most operations.

--- src/main/java/eu/cessda/cvs/security/ActionType.java
+++ src/main/java/eu/cessda/cvs/security/ActionType.java
@@ -17,9 +17,7 @@ package eu.cessda.cvs.security;
 
 import eu.cessda.cvs.domain.enumeration.AgencyRole;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.EnumSet;
 import java.util.Set;
 
 /**
@@ -27,36 +25,36 @@ import java.util.Set;
  * Each enums contains, types of AgencyRole allowed to perform the action
  */
 public enum ActionType {
-    CREATE_CV( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    EDIT_CV( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    EDIT_DDI_CV( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    EDIT_IDENTITY_CV( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    EDIT_VERSION_INFO_CV( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    EDIT_NOTE_CV( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    DELETE_CV( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
+    CREATE_CV(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    EDIT_CV(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    EDIT_DDI_CV(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    EDIT_IDENTITY_CV(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    EDIT_VERSION_INFO_CV(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    EDIT_NOTE_CV(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    DELETE_CV(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
     // ADD_USAGE_CV( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL))),
-    ADD_TL_CV( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    FORWARD_CV_SL_STATUS_REVIEW( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    FORWARD_CV_SL_STATUS_READY_TO_TRANSLATE( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    FORWARD_CV_SL_STATUS_PUBLISH( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    FORWARD_CV_TL_STATUS_REVIEW( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    FORWARD_CV_TL_STATUS_READY_TO_PUBLISH( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
+    ADD_TL_CV(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    FORWARD_CV_SL_STATUS_REVIEW(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    FORWARD_CV_SL_STATUS_READY_TO_TRANSLATE(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    FORWARD_CV_SL_STATUS_PUBLISH(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    FORWARD_CV_TL_STATUS_REVIEW(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    FORWARD_CV_TL_STATUS_READY_TO_PUBLISH(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
     // FORWARD_CV_TL_STATUS_PUBLISH( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_TL))),
-    CREATE_CODE( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    EDIT_CODE( new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    REORDER_CODE(new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    DEPRECATE_CODE(new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    DELETE_CODE(new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    ADD_TL_CODE(new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    EDIT_TL_CODE(new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    DELETE_TL_CODE(new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT))),
-    CREATE_NEW_CV_SL_VERSION(new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT))),
-    CREATE_NEW_CV_TL_VERSION(new HashSet<>(Arrays.asList(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)));
+    CREATE_CODE(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    EDIT_CODE(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    REORDER_CODE(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    DEPRECATE_CODE(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    DELETE_CODE(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    ADD_TL_CODE(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    EDIT_TL_CODE(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    DELETE_TL_CODE(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT)),
+    CREATE_NEW_CV_SL_VERSION(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_SL, AgencyRole.ADMIN_CONTENT)),
+    CREATE_NEW_CV_TL_VERSION(EnumSet.of(AgencyRole.ADMIN, AgencyRole.ADMIN_TL, AgencyRole.ADMIN_CONTENT));
 
-    private final Set<AgencyRole> agencyRoles;
+    private final EnumSet<AgencyRole> agencyRoles;
 
-    ActionType(Set<AgencyRole> agencyRoles) {
-        this.agencyRoles = Collections.unmodifiableSet(agencyRoles);
+    ActionType(EnumSet<AgencyRole> agencyRoles) {
+        this.agencyRoles = agencyRoles;
     }
 
     /**
