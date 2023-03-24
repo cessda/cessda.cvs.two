77eb3b3e01c7b82905c284de1bf6ab88fc26608f
Martin Šeleng <pakoselo@users.noreply.github.com>
2023-03-24 19:34:07 +0100

    Correct the rights for different roles

--- src/main/java/eu/cessda/cvs/config/SecurityConfiguration.java
+++ src/main/java/eu/cessda/cvs/config/SecurityConfiguration.java
@@ -108,7 +108,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .antMatchers("/management/health").permitAll()
             .antMatchers("/management/info").permitAll()
             .antMatchers("/management/prometheus").permitAll()
-            .antMatchers("/management/**").hasAuthority(AuthoritiesConstants.ADMIN)
+            .antMatchers("/management/**").hasAnyAuthority(AuthoritiesConstants.ADMIN, AuthoritiesConstants.ADMIN_TECHNICAL)
         .and()
             .apply(securityConfigurerAdapter());
         // @formatter:on
