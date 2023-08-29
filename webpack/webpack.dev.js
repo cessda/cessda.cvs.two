9b07a32e0546a0c4f54ed4cf97b1ff523b72203f
Matthew Morris <matthew.morris@cessda.eu>
2023-08-29 14:33:11 +0200

    Remove webpack-notifier

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -18,7 +18,6 @@ const writeFilePlugin = require('write-file-webpack-plugin');
 const webpackMerge = require('webpack-merge');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
-const WebpackNotifierPlugin = require('webpack-notifier');
 const path = require('path');
 const sass = require('sass');
 
@@ -120,11 +119,7 @@ module.exports = (options) => webpackMerge(commonConfig({ env: ENV }), {
         new writeFilePlugin(),
         new webpack.WatchIgnorePlugin([
             utils.root('src/test'),
-        ]),
-        new WebpackNotifierPlugin({
-            title: 'JHipster',
-            contentImage: path.join(__dirname, 'logo-jhipster.png')
-        })
+        ])
     ].filter(Boolean),
     mode: 'development'
 });
