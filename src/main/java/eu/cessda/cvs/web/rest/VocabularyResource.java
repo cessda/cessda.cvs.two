01c539ed7eb709a03775fb5111c5348c883dfcde
Martin Šeleng <pakoselo@users.noreply.github.com>
2023-10-12 08:56:39 +0200

    cleaning code

--- src/main/java/eu/cessda/cvs/web/rest/VocabularyResource.java
+++ src/main/java/eu/cessda/cvs/web/rest/VocabularyResource.java
@@ -33,7 +33,6 @@ import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
@@ -45,7 +44,6 @@ import javax.validation.Valid;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
