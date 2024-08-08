f474f92ad2597708c9d2dc55d254f4edaff8514b
Matthew Morris <matthew.morris@cessda.eu>
2024-08-08 10:42:49 +0200

    Typed Form Migration: Migrate agency update component

--- src/main/webapp/app/agency/agency-update.component.ts
+++ src/main/webapp/app/agency/agency-update.component.ts
@@ -15,7 +15,7 @@
  */
 import { Component, OnInit } from '@angular/core';
 import { HttpEventType, HttpResponse } from '@angular/common/http';
-import { UntypedFormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { JhiDataUtils, JhiEventManager, JhiEventWithContent, JhiFileLoadError } from 'ng-jhipster';
@@ -43,9 +43,9 @@ export class AgencyUpdateComponent implements OnInit {
   };
 
   editForm = this.fb.group({
-    id: [],
-    name: [null, [Validators.required, Validators.maxLength(240)]],
-    link: [
+    id: new FormControl<number | null>(null),
+    name: new FormControl<string | null>(null, [Validators.required, Validators.maxLength(240)]),
+    link: new FormControl<string | null>(
       null,
       [
         Validators.required,
@@ -53,12 +53,12 @@ export class AgencyUpdateComponent implements OnInit {
           '(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})',
         ),
       ],
-    ],
-    description: [],
-    licenseId: [],
-    uri: [null, [Validators.maxLength(255)]],
-    uriCode: [null, [Validators.maxLength(255)]],
-    canonicalUri: [null, [Validators.maxLength(255)]],
+    ),
+    description: new FormControl<string | null>(null),
+    licenseId: new FormControl<number | null>(null),
+    uri: new FormControl<string | null>(null, [Validators.maxLength(255)]),
+    uriCode: new FormControl<string | null>(null, [Validators.maxLength(255)]),
+    canonicalUri: new FormControl<string | null>(null, [Validators.maxLength(255)]),
   });
 
   constructor(
@@ -68,7 +68,7 @@ export class AgencyUpdateComponent implements OnInit {
     protected agencyService: AgencyService,
     protected activatedRoute: ActivatedRoute,
     protected fileUploadService: FileUploadService,
-    private fb: UntypedFormBuilder,
+    private fb: FormBuilder,
   ) {}
 
   ngOnInit(): void {
@@ -101,9 +101,9 @@ export class AgencyUpdateComponent implements OnInit {
   }
 
   setFileData(event: Event, field: string, isImage: boolean): void {
-    this.dataUtils.loadFileToForm(event, this.editForm, field, isImage).subscribe(null, (err: JhiFileLoadError) => {
+    this.dataUtils.loadFileToForm(event, this.editForm, field, isImage).subscribe({error: (err: JhiFileLoadError) => {
       this.eventManager.broadcast(new JhiEventWithContent<AlertError>('cvsApp.error', { ...err, key: 'error.file.' + err.key }));
-    });
+    }});
   }
 
   previousState(): void {
@@ -123,14 +123,14 @@ export class AgencyUpdateComponent implements OnInit {
 
   private createFromForm(): Agency {
     const agency = createNewAgency({
-      id: this.editForm.get(['id'])!.value,
-      name: this.editForm.get(['name'])!.value,
-      link: this.editForm.get(['link'])!.value,
-      description: this.editForm.get(['description'])!.value,
-      licenseId: +this.editForm.get(['licenseId'])!.value,
-      uri: this.editForm.get(['uri'])!.value,
-      uriCode: this.editForm.get(['uriCode'])!.value,
-      canonicalUri: this.editForm.get(['canonicalUri'])!.value,
+      id: this.editForm.controls.id.value!,
+      name: this.editForm.controls.name.value!,
+      link: this.editForm.controls.link.value!,
+      description: this.editForm.controls.description.value!,
+      licenseId: +this.editForm.controls.licenseId.value!,
+      uri: this.editForm.controls.uri.value!,
+      uriCode: this.editForm.controls.uriCode.value!,
+      canonicalUri: this.editForm.controls.canonicalUri.value!,
     });
     if (agency.licenseId) {
       agency.license = this.licences.filter(l => l.id === agency.licenseId)[0].name;
